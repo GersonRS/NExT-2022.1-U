358125678b265ad179af2a03d036f7822092920b
GersonRS <gersonrs@live.com>
2022-05-19 22:17:10 -0300

    atualizando app

--- aula12/src/App.java
+++ aula12/src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
+import pratica.Atendente;
+import pratica.ItemPedido;
+import pratica.PedidoBasico;
+import pratica.Produto;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Atendente at1 = new Atendente();
+
+        PedidoBasico pb1 = new PedidoBasico();
+        ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
+
+        listaProdutos.add(new Produto());
+        listaProdutos.add(new Produto());
+        listaProdutos.add(new Produto());
+        listaProdutos.add(new Produto());
+
+        ItemPedido ip = new ItemPedido(10, listaProdutos.get(0));
+
+        pb1.getListaPedidos().add(ip);
 
     }
 }
